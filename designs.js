d3e8f22bede236a4e0b66172c7bb1144c4f3f94a
salmonroesushi <nnicol@umassd.edu>
2018-04-05 11:26:34 -0400

    using geojson

--- designs.js
+++ designs.js
@@ -1,21 +1,145 @@
-//function createVis(errors, mapData, spendingData)  {
-function createVis(errors, map_data, state_abbrev, listings_data)  {
+function createVis(errors, map_data, state_abbrev, listings_data, state_cnt, cat_cnt)  {
   var features = map_data.features;
   console.log(features);
   console.log(state_abbrev);
   console.log(listings_data);
+  console.log(state_cnt);
+  console.log(cat_cnt);
+  
+  state_counts = mergeAbbrevCount(state_abbrev, state_cnt);
+  map_data.features.forEach(function(feature) {
+    feature.properties['JOB_TOTAL'] = state_counts[feature.properties.NAME];
+  });
+  
+  var val_range = d3.extent(map_data.features.map(x => x.properties.JOB_TOTAL));
+  var color_scale = d3.scaleSequential(d3.interpolateReds)
+    .domain(val_range);
+  
+  // create leaflet map using map_data
+  var map = new L.Map('leaflet_map', {center: [37, -95], zoom: 4})
+    .addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
+  var geojson;
+  
+  // functions inside functions!
+  // to handle events on the features
+  
+  // defining events on geoJSON for each feature/state
+  function featureEvents(feature, layer) {
+    layer.on({
+      mouseover: featureHighlight,
+      mouseout: featureResetHighlight
+      //click: zoomToFeature
+    });
+  }
+
+  // defining default style for each feature/state
+  function featureStyle(feature) {
+    return {
+      fillColor: color_scale(feature.properties.JOB_TOTAL),
+      fillOpacity: 0.5,
+      color: 'black',
+      weight: 1
+    };
+  }
+
+  // on mouseover event
+  function featureHighlight(e) {
+    var layer = e.target;
+    
+    layer.setStyle({
+      weight: 3,
+      color: 'white'
+    });
+    
+    layer.bringToFront();
+  }
+
+  // on mouseout event
+  function featureResetHighlight(e) {
+    geojson.resetStyle(e.target);
+  }
+  
+  // add map_data to leaflet map
+  geojson = L.geoJson(map_data, {
+    style: featureStyle,
+    onEachFeature: featureEvents
+  }).addTo(map);
+  
+  // add additional data for each state on the path
+  // can't do inside onEachFeature because path is undefined
+  geojson.eachLayer(function(layer) {
+    layer._path.setAttribute('state', layer.feature.properties.NAME);
+    layer._path.setAttribute('job_total', layer.feature.properties.JOB_TOTAL);
+    g1 = layer;
+  });
+  g2 = geojson;
+  
+  // test
+  /*
+  geojson.eachLayer(function(layer) {
+    if(!(layer.feature.properties.NAME == 'Texas')) {
+      layer._path.setAttribute('fill', 'black');
+      layer._path.setAttribute('opacity', 1.0);
+    }
+  });
+  */
 }
 
-function assignment3() {
+
+
+// turn abbreviation object and state job count object into new object
+function mergeAbbrevCount(abbrev, cnt) {
+  var keys = Object.keys(cnt);
+  var return_obj = {};
+  keys.forEach(function(key) {
+    return_obj[abbrev[key]] = cnt[key];
+  });
+  
+  return return_obj;
+}
+
+// change lat long to points
+function projectPoint(map) {
+  return function(lng, lat) {
+    var point = map.latLngToLayerPoint(new L.latLng(lat, lng));
+    this.stream.point(point.x, point.y);
+  }
+}
+
+// updating SVG map overlay
+function updateMap(map, map_data, canvas, group) {
+  return function() {
+    var transform = d3.geoTransform({point: projectPoint(map)});
+    var path = d3.geoPath().projection(transform);
+    
+    var bounds = path.bounds(map_data);
+    
+    var boundsTopLeft = bounds[0];
+    var boundsBottomRight = bounds[1];
+    
+    group.selectAll('path')
+      .attr('d', path)
+      .attr('transform', 'translate(' + -boundsTopLeft[0] + ',' + -boundsTopLeft[1] + ')');
+    
+    canvas
+      .attr('width', boundsBottomRight[0] - boundsTopLeft[0])
+      .attr('height', boundsBottomRight[1] - boundsTopLeft[1])
+      .style('left', boundsTopLeft[0] + 'px')
+      .style('top', boundsTopLeft[1] + 'px');
+  };
+}
+
+function projectLoad() {
   // uncomment the cdn.rawgit.com versions and comment the cis.umassd.edu versions if you require all https data
   d3.queue()
     .defer(d3.json, "https://raw.githubusercontent.com/salmonroesushi/dsc530/master/data/gz_2010_us_040_00_20m.json")
     .defer(d3.json, "https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json") // dict with key as abbrev and value as full name
     .defer(d3.csv, "https://raw.githubusercontent.com/salmonroesushi/dsc530/master/data/listings.csv")
+    .defer(d3.json, "https://raw.githubusercontent.com/salmonroesushi/dsc530/master/data/state_cnt.json")
+    .defer(d3.json, "https://raw.githubusercontent.com/salmonroesushi/dsc530/master/data/cat_cnt.json")
     .await(createVis);
 }
 
-window.onload = assignment3;
-var g_test;
-var width = 960,
-    height = 500;
+window.onload = projectLoad;
+var g1;
+var g2;
