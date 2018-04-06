8bb358dc8d7b963e15a88e62542b06763b903e18
salmonroesushi <nnicol@umassd.edu>
2018-04-05 22:42:16 -0400

    job category control filter

--- designs.js
+++ designs.js
@@ -1,30 +1,54 @@
-function createVis(errors, map_data, state_abbrev, listings_data, state_cnt, cat_cnt)  {
+function createVis(errors, map_data, state_abbrev, listings_data, state_jobs, job_categories)  {
   var features = map_data.features;
-  console.log(features);
-  console.log(state_abbrev);
-  console.log(listings_data);
-  console.log(state_cnt);
-  console.log(cat_cnt);
+  //console.log(features);
+  //console.log(state_abbrev);
+  //console.log(listings_data);
+  //console.log(state_jobs);
+  //console.log(job_categories);
   
-  state_counts = mergeAbbrevCount(state_abbrev, state_cnt);
+  // create object with state:jobcount to add to feature properties
+  state_counts = mergeAbbrevCount(state_abbrev, state_jobs);
+  
+  // create object with state:abbrev to add to feature properties
+  var state_invert = {};
+  for(var key in state_abbrev) {
+    state_invert[state_abbrev[key]] = key;
+  }
+  
+  // add additional properties to geoJSON features
   map_data.features.forEach(function(feature) {
-    feature.properties['JOB_TOTAL'] = state_counts[feature.properties.NAME];
+    feature.properties['JOBS'] = (state_counts[feature.properties.NAME] === undefined ? {total: 0} : state_counts[feature.properties.NAME]);
+    feature.properties['ABBREVIATION'] = state_invert[feature.properties.NAME];
   });
   
-  val_range = d3.extent(map_data.features.map(x => x.properties.JOB_TOTAL));
+  // initial color scale using absolute jobs
+  /*
+  val_range = d3.extent(map_data.features.map(x => x.properties.JOBS.total));
   color_scale = d3.scaleSequential(d3.interpolateReds)
     .domain(val_range);
+  */
+  color_scale = getColorScale(map_data, 'total');
   
   // define variables used later
   // create leaflet map using map_data
-  map = new L.Map('leaflet_map', {center: [37, -95], zoom: 4})
-    .addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
+  map = new L.Map('leaflet_map', {
+    center: [39, -96],
+    zoom: 4,
+    zoomControl: false,
+    doubleClickZoom: false
+  });
+  map.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
+  
+  // add zoom button to bottomleft corner
+  //L.control.zoom({position: 'bottomleft'}).addTo(map);
+  map.addControl(new L.control.zoom({position: 'bottomleft'}));
   
   // add map_data to leaflet map
   geojson = L.geoJson(map_data, {
     style: featureStyle,
     onEachFeature: featureEvents
-  }).addTo(map);
+  });
+  geojson.addTo(map);
   
   // add additional data for each state on the path
   // can't do inside onEachFeature because path is undefined
@@ -34,20 +58,81 @@ function createVis(errors, map_data, state_abbrev, listings_data, state_cnt, cat
   });
   
   // adding custom control to select job categories
-  control = L.control({position: 'topright'});
+  filter_control = createFilterControl(map, map_data, job_categories);
+  filter_control.addTo(map);
+}
+
+function getColorScale(map_data, category) {
+  var range = d3.extent(map_data.features.map(x => x.properties.JOBS[category]));
+  var scale = d3.scaleSequential(d3.interpolateReds)
+    .domain(range);
+  
+  return scale;
+}
+
+function updateMapCategory(map_data, category)
+{
+  if(category === 'none') {
+    category = 'total';
+  }
+  var scale = getColorScale(map_data, category);
+  
+  geojson.eachLayer(function(layer) {
+    layer.setStyle( {
+      fillColor: scale((layer.feature.properties.JOBS[category] === undefined ? 0 : layer.feature.properties.JOBS[category])),
+      fillOpacity: 1.0
+    });
+  });
+}
+
+function createFilterControl(map, map_data, categories) {
+  var control = L.control({position: 'topright'});
   
   control.onAdd = function(map) {
-    this._div = L.DomUtil.create('div', 'job_filter');
-    this.update();
+    this._div = filterControlBody(map_data, categories);
     return this._div;
-  }
+  };
   
-  control.update = function(properties) {
-    this._div.innerHTML = '<h3>' + (properties === undefined ? '' : properties.NAME) + '</h3>';
-    console.log(properties);
-  }
+  return control;
+}
+
+function filterControlBody(map_data, categories) {
+  // create DOM elements
+  var filter = L.DomUtil.create('div', 'job_filter');
   
-  control.addTo(map);
+  // text
+  var heading = L.DomUtil.create('h4');
+  heading.innerText = 'Select a job category';
+  filter.appendChild(heading);
+  
+  // add dropdown and options
+  var dropdown = L.DomUtil.create('select');
+  dropdown.id = 'job_category_dropdown';
+  
+  // add a default None option for category dropdown
+  var cat_none = L.DomUtil.create('option');
+  cat_none.setAttribute('value', 'none');
+  cat_none.innerText = 'None';
+  dropdown.appendChild(cat_none);
+  cat_none.selected = true;
+  
+  // add options for all job categories
+  categories.forEach(function(key) {
+    var cat = key.replace(/-/g, ' ').replace(/^(.)|\s(.)/g, m => m.toUpperCase());
+    var cat_opt = L.DomUtil.create('option');
+    cat_opt.setAttribute('value', key);
+    cat_opt.innerText = cat;
+    dropdown.appendChild(cat_opt);
+  });
+  
+  dropdown.addEventListener('change', function(event) {
+    var cat_selected = event.target[event.target.selectedIndex].value;
+    updateMapCategory(map_data, cat_selected);
+  });
+  
+  filter.appendChild(dropdown);
+  
+  return filter;
 }
 
 // defining events on geoJSON for each feature/state
@@ -55,14 +140,14 @@ function featureEvents(feature, layer) {
   layer.on({
     mouseover: featureMouseover,
     mouseout: featureMouseout,
-    click: featureClick
+    //click: clickEvent
   });
 }
 
 // defining default style for each feature/state
 function featureStyle(feature) {
   return {
-    fillColor: color_scale(feature.properties.JOB_TOTAL),
+    fillColor: color_scale(feature.properties.JOBS.total),
     fillOpacity: 0.5,
     color: 'black',
     weight: 1
@@ -94,29 +179,6 @@ function featureMouseout(e) {
   });
 }
 
-// testing click functions
-function featureClick(e) {
-  var layer = e.target;
-  
-  if(state_selected != layer) {
-    if(state_selected !== undefined) {
-      geojson.resetStyle(state_selected);
-    }
-    
-    layer.setStyle({
-      fillColor: 'red',
-      fillOpacity: 1
-    });
-    
-    state_selected = layer;
-  }
-  else {
-    geojson.resetStyle(state_selected);
-    state_selected = undefined;
-  }
-  control.update(layer.feature.properties);
-}
-
 // turn abbreviation object and state job count object into new object
 function mergeAbbrevCount(abbrev, cnt) {
   var keys = Object.keys(cnt);
@@ -128,45 +190,14 @@ function mergeAbbrevCount(abbrev, cnt) {
   return return_obj;
 }
 
-// change lat long to points
-function projectPoint(map) {
-  return function(lng, lat) {
-    var point = map.latLngToLayerPoint(new L.latLng(lat, lng));
-    this.stream.point(point.x, point.y);
-  }
-}
-
-// updating SVG map overlay
-function updateMap(map, map_data, canvas, group) {
-  return function() {
-    var transform = d3.geoTransform({point: projectPoint(map)});
-    var path = d3.geoPath().projection(transform);
-    
-    var bounds = path.bounds(map_data);
-    
-    var boundsTopLeft = bounds[0];
-    var boundsBottomRight = bounds[1];
-    
-    group.selectAll('path')
-      .attr('d', path)
-      .attr('transform', 'translate(' + -boundsTopLeft[0] + ',' + -boundsTopLeft[1] + ')');
-    
-    canvas
-      .attr('width', boundsBottomRight[0] - boundsTopLeft[0])
-      .attr('height', boundsBottomRight[1] - boundsTopLeft[1])
-      .style('left', boundsTopLeft[0] + 'px')
-      .style('top', boundsTopLeft[1] + 'px');
-  };
-}
-
 function projectLoad() {
   // uncomment the cdn.rawgit.com versions and comment the cis.umassd.edu versions if you require all https data
   d3.queue()
     .defer(d3.json, "https://raw.githubusercontent.com/salmonroesushi/dsc530/master/data/gz_2010_us_040_00_20m.json")
     .defer(d3.json, "https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json") // dict with key as abbrev and value as full name
     .defer(d3.csv, "https://raw.githubusercontent.com/salmonroesushi/dsc530/master/data/listings.csv")
-    .defer(d3.json, "https://raw.githubusercontent.com/salmonroesushi/dsc530/master/data/state_cnt.json")
-    .defer(d3.json, "https://raw.githubusercontent.com/salmonroesushi/dsc530/master/data/cat_cnt.json")
+    .defer(d3.json, "https://raw.githubusercontent.com/salmonroesushi/dsc530/global/data/state_jobs.json")
+    .defer(d3.json, "https://raw.githubusercontent.com/salmonroesushi/dsc530/global/data/job_categories.json")
     .await(createVis);
 }
 
@@ -177,9 +208,6 @@ window.onload = projectLoad;
 var map;
 var geojson;
 var color_scale;
-var control;
+var filter_control;
 var state_selected;
 var val_range;
-
-var g1;
-var g2;
